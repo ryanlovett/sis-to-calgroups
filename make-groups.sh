63ce3d95cd2de11cdfcd02809c2c093b48673a82
ryanlovett <rylo@berkeley.edu>
2019-04-11 17:14:14 -0700

    Be verbose when fetching sis data.
    We don't want to timeout during build.

--- make-groups.sh
+++ make-groups.sh
@@ -31,7 +31,7 @@ outdir=`mktemp -d`
 
 for constituent in enrolled waitlisted gsis instructors ; do
 	echo sis: getting $year $term $class $constituent
-	sis -f ${sis_creds} people -y $year -s $term -n $class \
+	sis -v -f ${sis_creds} people -y $year -s $term -n $class \
 		-c $constituent > \
 		${outdir}/${year}-${term}-${class}-${constituent}.txt
 done
